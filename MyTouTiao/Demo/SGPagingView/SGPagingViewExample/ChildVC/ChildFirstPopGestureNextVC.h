2adbab94aa6e6d554578b087235dc8bfbed684a7
lvxinghua <2445888943@qq.com>
2018-07-20 10:42:24 +0800

    多标题滑动菜单

---
+++ MyTouTiao/Demo/SGPagingView/SGPagingViewExample/ChildVC/ChildFirstPopGestureNextVC.h
@@ -0,0 +1,13 @@
+//
+//  ChildFirstPopGestureNextVC.h
+//  SGPagingViewExample
+//
+//  Created by kingsic on 2017/11/28.
+//  Copyright © 2017年 Sorgle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChildFirstPopGestureNextVC : UIViewController
+
+@end
