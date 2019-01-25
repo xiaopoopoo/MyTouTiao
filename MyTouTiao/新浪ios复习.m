8037a5f5c82c192daa6e04f2bb1062c183ade4f0
xiaopoopoo <229389099@qq.com>
2019-01-26 07:37:48 +0800

    fix

---
+++ MyTouTiao/新浪ios复习.m
@@ -0,0 +1,717 @@
+1-1 课程导学
+深入ios底层技术机制，原理
+完善ios技术知识体系
+中级 ：ui视图 oc语言 runtime 内存，block,多线程，runloop，网络，
+高级：设计模式 架构／框架 算法 第三方使用
+ui视图：
+uitableview 重用机制理解 运用 数据源多线程操作同步问题
+事件传递，视图响应
+图像显示原理 控件是如何转换为相素点的 理解ui卡顿 掉帧原因
+ui绘制原理／异步绘制
+离屏渲染
+
+oc语言
+分类 实现机制 原理
+关联对象 分类添加变量
+扩展 代理
+kvo,kvc 系统实现原理机制 设计模式
+nsnotification 机制实现原理
+属情关键字
+
+runtime
+对象，类对象，元类对象分别是什么
+消息传递机制是怎样的
+消息转发流程是怎样的
+方法缓存机制是怎样运行的
+method-swizzling runtime的技术实际应用
+动态添加方法
+
+内存管理
+arc
+mrc
+引用计数机制
+弱引用表
+autoreleasepool
+循环引用
+
+block
+block的本质
+截获变量特性
+__block修饰的本质
+block的内存管理
+循环引用
+
+多线程
+gcd
+nsoperation/nsoperationqueue
+nsthread
+线程同步，资源共享问题
+三种技术怎么解决线程同步，资源共享问题，几种线程有什么特点，区别
+互斥锁，自旋锁，递归锁
+
+runloop
+什么是runloop
+mode/source /timer/observer
+事件循环机制是怎样的
+runloop与nstimer的关系
+runloop与线程之间的关系
+如何实现一个常驻线程
+
+网络
+http协议 get post区别
+https与网络安全 建立链接的流程
+tcp/udp 三次握手 四次挥手
+dns解析
+session/cookie
+
+设计模式
+六大设计原则
+责任链
+桥接
+适配器
+单例
+命令
+
+架构／框架
+图片缓存框架
+时长统计框架
+复杂页面架构
+客户端整体架构
+
+算法
+字符串反转
+链表反转
+有序数组合并
+hash算法
+查找两个子视图的共同父视图
+求无序数组当中的中位数
+
+第三库
+afnetworking
+sdwebimageview
+reactive cocoa
+asyncdisplaykit
+
+2-1 各大公司初中高IOS工程师岗位技能要求
+初级工程师 
+会用 精通oc语言基础 正确使用oc分类扩展kvo kvc
+uikit cocoa framework 自定义ui控件
+网络通信机制，常用数据传输协议的理解 http https 对称加密 非对称 json xml
+主流开源框架使用经验 afnetworking 图片异步下载 使用心得
+中级工程师
+为什么要这样用
+编程基础，数据结构，算法解决实际问题
+深入理解oc语言机制 runtime 内存管理要求 网络多线程 图形用户ui
+常用设计模式，框架，架构
+良好分析解决问题能力 最好的学习对象就是源码，深入了解解决问题思路 逻辑
+高级工程师
+创造性解决开发中关键问题和技术难题
+设备流量，性能，电量等调优如外卖 今日头条，视频滑动流畅性
+较强的软件设计能力 对超级复杂页面架构设计
+ios内部原理有深刻了解
+资深工程师
+精通高性能编程，性能调优 在前期设计会考虑架构的问题 如字符串大量遍历，选技较优算法
+灵活运用数据结构，算法解决复杂程序设计问题
+提供性能优化，日志搜集，统计分析方案
+架构，模块设计
+
+2-2 如何编写一份让HR或面试官眼前一亮的简历？
+遵从：简洁性，真实性，全面性
+简洁性：排版清晰，亮点，优势 5年以上的简历不要超过三页，挑选重要突出表达，无关的不需要表达
+真实性：可以包装，但不能作假 量化指标去说明问题 如项目表达闪退量的变化，开发前和开发后crash量小于多少，线上bug数变化，开发成本节约了多少，一定有一个量去定论
+不要用pv,uv去说，这是用于产品方面的
+全面性：个人基本信息邮箱，联系方式很重要，履历过公司要全面，尽量有一个十分亮点的体现
+简历四要素：
+基本信息 姓名 现居住地 学历 工作年限 专业 邮箱用hotmail gmail比较好 电话 有头像也不错
+工作经历 在哪个时间段就职哪个公司什么职位 角色变化体现进步
+项目经验 列举比较有亮点的2-3个项目，在项目中承担的角色 是主导 参与 核心研发者
+擅长技能 项目背景，方案，效果 了解背景交待 技术方案 效果是方案的验证
+
+主导／参与／核心参与xxx项目
+背景 改善原有代码，不易维护，需求快速迭代
+方案，合理运用桥接，命令设计模式，对原有业务进行分层，解耦，降低代码重合度
+效果 体现出简历全面性 重构前崩溃量 重构后崩溃 重构后清除多少行代码
+
+擅长技能
+尽可能突出亮点重点
+擅长feed流性能优化，性能优化
+是精通，掌握，精通，了解， 中高级要写精通
+能通过多线程，runtime机制解决一些难点问题
+还有对其它语言，也实事求事，熟悉，精通。
+简历是一块敲门砖
+
+
+3-1 UITableView 相关面试问题
+UITableView的重用机制和原理是什么样的
+引出事件传递&视图响应的机制和流程
+图像显示原理，更好的理解ui卡顿和掉帧的原因
+绘制原理&异步绘制，异步绘制是解决ui滑动性能的技术解决方案，什么是离屏渲染，为什么要规避离屏渲染
+
+UITableView 相关
+重用机制
+cell = [tableview dequeuereusablecellwithidentifer:identifer];
+
+A1  所有在屏外  
+A2 一部份显示在屏幕中
+A3  所有在屏内
+A4  所有在屏内
+A5  所有在屏内
+A6 一部份显示在屏幕中
+A7 所有在屏外
+多线程下，数据源同步问题
+在继续滑动的情况下，a1是所有都在屏幕外的，会放入到重用池中，当a7要进入屏幕显示的时候
+会从重用池中跟据identifer拿内存复用，如果a1，a7都是同一个标识符，a7就可以在重用池中用
+a1的内存
+
+下面通过自定义ui控件，字母索引条，来体现在ui重用机制
+
+
+ViewReusePool 类，代表视图的重用池
+
+ViewReusePool.h
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+// 实现重用机制的类
+@interface ViewReusePool : NSObject
+
+// 从重用池当中取出一个可重用的view
+- (UIView *)dequeueReusableView;
+
+// 向重用池当中添加一个视图
+- (void)addUsingView:(UIView *)view;
+
+// 重置方法，将当前使用中的视图移动到可重用队列当中
+- (void)reset;
+
+@end
+
+ViewReusePool.m
+
+#import "ViewReusePool.h"
+
+@interface ViewReusePool ()//创建了一个扩展，在里面添加了两个变量
+// 等待使用的队列，集合实现
+@property (nonatomic, strong) NSMutableSet *waitUsedQueue;
+// 使用中的队列，集合实现
+@property (nonatomic, strong) NSMutableSet *usingQueue;
+@end
+
+@implementation ViewReusePool
+//创建两个对列的内存
+- (id)init{
+    self = [super init];
+    if (self) {
+        _waitUsedQueue = [NSMutableSet set];
+        _usingQueue = [NSMutableSet set];
+    }
+    return self;
+}
+
+- (UIView *)dequeueReusableView{
+    UIView *view = [_waitUsedQueue anyObject];//从等待的对列当中随机取出一个对象
+    if (view == nil) {//如果没有，返回view
+        return nil;
+    }
+    else{
+        // 进行队列移动
+        [_waitUsedQueue removeObject:view];//如果有，则把这个view从这个等待队列移掉
+        [_usingQueue addObject:view];//添加到正在使用的队列中
+        return view;//返回这个视图的内存
+    }
+}
+
+- (void)addUsingView:(UIView *)view
+{
+    if (view == nil) {//如果是空的，直接返回
+        return;
+    }
+    
+    // 如果有则添加视图到使用中的队列
+    [_usingQueue addObject:view];
+}
+
+- (void)reset{
+    UIView *view = nil;
+    while ((view = [_usingQueue anyObject])) {//遍列使用队列中的所有视图，如果有
+        // 从使用中队列移除
+        [_usingQueue removeObject:view];
+        // 加入等待使用的队列
+        [_waitUsedQueue addObject:view];
+    }
+}
+
+@end
+
+
+
+IndexedTableView类，是uitableview的子类，实现了带索引条的类
+
+IndexedTableView.h
+
+#import <UIKit/UIKit.h>
+
+
+@protocol IndexedTableViewDataSource <NSObject>//定义了一个索引条的数据源协议
+
+// 获取一个tableview的字母索引条数据的方法，为字母条提供显示哪些字母
+- (NSArray <NSString *> *)indexTitlesForIndexTableView:(UITableView *)tableView;
+
+@end
+
+@interface IndexedTableView : UITableView
+@property (nonatomic, weak) id <IndexedTableViewDataSource> indexedDataSource;//定义一个weak属性关建字的数据源
+@end
+
+
+
+IndexedTableView.m
+//
+//  IndexedTableView.m
+//  IndexedBar
+//
+//  Created by yangyang38 on 2018/2/14.
+//  Copyright © 2018年 yangyang. All rights reserved.
+//
+
+#import "IndexedTableView.h"
+#import "ViewReusePool.h"
+@interface IndexedTableView ()//在扩展当中定义了两个成员变量
+{
+    UIView *containerView;//容器view，用来装载所有字母的控件
+    ViewReusePool *reusePool;//声明字母重用池
+}
+@end
+
+@implementation IndexedTableView
+
+- (void)reloadData{//重写reloaddata方法，在其中实现字母条的重用
+    [super reloadData];
+    
+    // 懒加载，创建容器的view，指定它的背景色为白色
+    if (containerView == nil) {
+        containerView = [[UIView alloc] initWithFrame:CGRectZero];
+        containerView.backgroundColor = [UIColor whiteColor];
+        
+        //把字母索引条容器插入当前视图的最上边，避免索引条随着table滚动，不写这句话，字母索引条会随着uitableview滚动而滚动
+        
+        [self.superview insertSubview:containerView aboveSubview:self];
+    }
+    
+    if (reusePool == nil) {//懒加载去创建重用池
+        reusePool = [[ViewReusePool alloc] init];
+    }
+    
+    // 标记所有视图为可重用状态，就是把正在重用队列中的所有视图移除掉，添加到等待重用队列中
+    [reusePool reset];
+    
+    // reload字母索引条
+    [self reloadIndexedBar];
+}
+
+- (void)reloadIndexedBar
+{
+    // 获取字母索引条的显示内容
+    NSArray <NSString *> *arrayTitles = nil;//用来表达字母索引条中的所有字母
+    if ([self.indexedDataSource respondsToSelector:@selector(indexTitlesForIndexTableView:)]) {//判断是否响应这个数据源方法
+        arrayTitles = [self.indexedDataSource indexTitlesForIndexTableView:self];//响应则实现它的代理，通过数据源提供方获取这个数据源的数组
+    }
+    
+    // 判断字母索引条是否为空
+    if (!arrayTitles || arrayTitles.count <= 0) {//如果说获取到的数据源为空
+        [containerView setHidden:YES];//没有数据源，设计容器隐藏
+        return;
+    }
+    
+    NSUInteger count = arrayTitles.count;//如果有数据
+    CGFloat buttonWidth = 60;
+    CGFloat buttonHeight = self.frame.size.height / count;//平分tableview获取高度
+    
+    for (int i = 0; i < [arrayTitles count]; i++) {
+        NSString *title = [arrayTitles objectAtIndex:i];//取现每个字母
+        
+        // 从重用池当中取一个Button出来
+        UIButton *button = (UIButton *)[reusePool dequeueReusableView];//取出一个等待重用池中的视图
+        // 如果没有可重用的Button重新创建一个
+        if (button == nil) {
+            button = [[UIButton alloc] initWithFrame:CGRectZero];
+            button.backgroundColor = [UIColor whiteColor];
+            
+            // 注册button到使用中的重用池当中
+            [reusePool addUsingView:button];
+            NSLog(@"新创建一个Button");
+        }
+        else{
+            NSLog(@"Button 重用了");
+        }
+        
+        // 添加button到父视图控件
+        [containerView addSubview:button];
+        [button setTitle:title forState:UIControlStateNormal];
+        [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        
+        // 设置button的坐标
+        [button setFrame:CGRectMake(0, i * buttonHeight, buttonWidth, buttonHeight)];
+    }
+    
+    [containerView setHidden:NO];//设置隐藏属性为no
+    containerView.frame = CGRectMake(self.frame.origin.x + self.frame.size.width - buttonWidth, self.frame.origin.y, buttonWidth, self.frame.size.height);
+}
+
+
+@end
+
+
+ 
+ViewController类，中使用字母索引条类
+
+ViewController.h 
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
+ViewController.m
+
+
+#import "ViewController.h"
+#import "IndexedTableView.h"
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate,IndexedTableViewDataSource>//自定义的数据源IndexedTableViewDataSource
+{
+    IndexedTableView *tableView;//带有索引条的tableview
+    UIButton *button;//点击一下button来reloadload看看在这个自定义字母索引的流程
+    NSMutableArray *dataSource;
+}
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //创建一个Tableview
+    tableView = [[IndexedTableView alloc] initWithFrame:CGRectMake(0, 60, self.view.frame.size.width, self.view.frame.size.height - 60) style:UITableViewStylePlain];
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    
+    // 设置table的索引数据源
+    tableView.indexedDataSource = self;
+    
+    [self.view addSubview:tableView];
+    
+    //创建一个按钮
+    button = [[UIButton alloc] initWithFrame:CGRectMake(0, 20, self.view.frame.size.width, 40)];
+    button.backgroundColor = [UIColor redColor];
+    [button setTitle:@"reloadTable" forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(doAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+    
+    // 数据源，1到99的数据
+    dataSource = [NSMutableArray array];
+    for (int i = 0; i < 100; i++) {
+        [dataSource addObject:@(i+1)];
+    }
+    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+#pragma mark IndexedTableViewDataSource
+
+- (NSArray <NSString *> *)indexTitlesForIndexTableView:(UITableView *)tableView{
+    
+    //奇数次调用返回6个字母，偶数次调用返回11个，这里声名静态的bool值变量
+    static BOOL change = NO;
+    
+    if (change) {
+        change = NO;
+        return @[@"A",@"B",@"C",@"D",@"E",@"F",@"G",@"H",@"I",@"J",@"K"];
+    }
+    else{
+        change = YES;
+        return @[@"A",@"B",@"C",@"D",@"E",@"F"];
+    }
+    
+}
+
+#pragma mark UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [dataSource count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *identifier = @"reuseId";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    //如果重用池当中没有可重用的cell，那么创建一个cell
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+    // 文案设置
+    cell.textLabel.text = [[dataSource objectAtIndex:indexPath.row] stringValue];
+    
+    //返回一个cell
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 40;
+}
+
+- (void)doAction:(id)sender{
+    NSLog(@"reloadData");
+    [tableView reloadData];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
+
+
+
+3-2 UI数据源同步 相关面试问题
+
+新闻，咨询类的app会涉及到数据源同步问题，如头条，微博
+
+场景：
+数据源列表a
+对数据源列表a进行删除操作，因为是用户手动删除，所以发生在主线程
+同时loadmore加载更多数据的操作也发生了，往往是在子线程当中后项刷新网络请求
+
+这个场景就涉及到了多线程对数据源的访问，需要考虑数据源同步问题
+
+两和方案
+方案1：并发访问 ，持续拷贝
+1。主线程作数据拷贝，拷贝的结果给子线程使用
+2。子线程的工作是对新数据的网络请求，数据解析，预排版等内容
+3。主线程删除一行数据，reloadui后数据就不见了，如果还有时间，主线程还会作一些其它的任务
+4。子线程返回请求的结果，再重新reloadui
+5。假设子线程使用的数据是主线程给的数据拷贝，那拷贝的数据是在主线程删除一行数据之前，也就是说子线程返回数据到主线程
+仍然包含主线程删除之前的数据，此时会产生删除的那条数据仍然在显示
+6。记录主线程之前删除的是哪一条数据，在子线程返回结果的时候再同步删除掉这条数据
+
+
+方案2：串行访问
+
+1。子线程，在进行网络请求和数据解析
+2。子线程作完请求和数据解析后放入到串行对列，对这些数据进行排版
+3。主线程此时要删除一行数据，把这个操作放到串行队列当中，等待子线程数据排版完成
+后才会同步数据删除，再readui
+
+方案1对内存的开销会有一定影响，方案2删除需要等子线程数据先排版，会有删除延时
+
+
+
+
+3-3 UI事件传递&响应 相关面试问题
+
+首先看一下uiview和calayer的关系
+
+1。uiview包含layer,backgroundcolor两个属性,backgroundcolor是对calayer的属性方法的包装
+2。layer包含calayer,calayer包含contents属性，contents决定了uiview的显示
+3。contents对应的backing store其实是一个位图，最终显示到屏幕上面的控件都是位图
+
+区别：
+uiview
+1。uiview为calayer提供显示的内容，如显示红色，白色
+2。以及负责处理触摸等事件，参与响应链
+
+calayer
+只是负责显示内容，通过contents去显示内容
+
+1。为什么uiview只负责提供显示内容，和响应链，calayer只负责显示
+这是一个单一设计原则，体现了职责上的分工
+
+1。事件传递与视图响应链的机制和流程
+
+viewa
+  |- viewb1
+  |- viewb2
+        |-viewc1
+            |-viewd
+        |-viewc2
+        
+当点击viewc2发生了什么？
+
+事件传递的两个方法：
+- (UIView*)hitTest:(CGPoint)point withEvent:(UIEvent *)event
+- (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event
+
+1.hitTest返回uiview，最终哪个视图响应事件，就把这个视图返回
+2. pointInside判断某一个点击位置是否在视图范围内，如果在就会返回yes
+
+事件传递的流程：
+点击屏幕
+  |(传递)
+uiapplication
+  |（传递）
+uiwindow
+  |(中会判断哪个视图最终响应)  
+hitTest （返回最终响应点击事件的视图）
+  | （hittest会调用pointinside）
+pointInside（判断点击事件是否在uiwindow范围内）
+  |  （如果点击事件在uiwindow范围内查找）
+subviews（遍列查找子视图，找出最终响应视件的子视图）
+      |遍列方式，是以倒序偏历查找的方式
+           |最后添加到uiwindows上的视图最优先被遍历
+               |在每个子uiview当中会再次调用hittest方法，uiview的子视图又会再调用hittest方法
+                    |如果hittest方法返回的是有值的，则这个视图就作为最终点击响应的视图返回
+                    
+以上流程简要概括：
+点击屏幕传递给uiapplication,传递给uiwindow，uiwindow根据hitest去返回最终响应的那个视图，
+hitest方法会先调用pointinside方法判断是否点击事件在uiwindow范围内，如果在，则去遍列uiwindow的
+subviews所有子视图，遍列是由最顶层先遍列的，每个遍列的子视图，都会调用hitest方法，如果这个子视图内还包含
+了子子视图，则选从子子视图开始遍列，如果子子视图的hittest方法返回有值，则这个视图为最终点击响应的视图
+
+
+hittest方法内部流程：
+1。判断视图是否alpha大于0.0.1,是否ishidden,是否userinteraction可点（成立继续第二步，否则返回nil）
+2。pointInside方法判断当前点击的点是否在视图范围内（成立继续第三步，否则返回nil）
+3。for(int i=v.subviews.count-1;i>0;i--){ sv=v.subviews[i]}
+4.sv再调用hittest，如果返回不为空，则返回这个视图sv，响应链结束，这个sv为最终响应的视图
+5。如果第3步遍列的子视图的hittest方法返回nil，未找到响应视图，就会把第二步的视图返回
+
+代码实战
+一个方形的按钮里面有一个圆形的区域，只有点击圆形区域有响应事件，圆形区域外点击无响应
+
+CustomButton类
+
+
+
+CustomButton.h
+
+#import <UIKit/UIKit.h>
+
+@interface CustomButton : UIButton
+
+@end
+
+
+
+
+
+
+
+CustomButton.m
+
+#import "CustomButton.h"
+
+@implementation CustomButton
+
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event
+{
+    if (!self.userInteractionEnabled ||
+        [self isHidden] ||
+        self.alpha <= 0.01) {
+        return nil;
+    }//判断自身如果是隐藏，透明，不可点击，返回nil
+    
+    if ([self pointInside:point withEvent:event]) {//如果在点击是在范围类
+        //NSEnumerationReverse倒序遍历当前对象的子视图，定义每个子视图为hit
+        __block UIView *hit = nil;
+        [self.subviews enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            // 当前点击的点转换一下坐标转换
+            CGPoint vonvertPoint = [self convertPoint:point toView:obj];
+            //调用子视图的hittest方法，返回不为空，则表明这个子视图接收了响应
+            hit = [obj hitTest:vonvertPoint withEvent:event];
+            // 如果找到了接受事件的对象，则停止遍历
+            if (hit) {
+                *stop = YES;//这里是停止遍历
+            }
+        }];
+        
+        if (hit) {
+            return hit;
+        }
+        else{
+            return self;
+        }
+    }
+    else{
+        return nil;
+    }
+}
+
+- (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event
+{
+    //取出点击位置的横纵坐标
+    CGFloat x1 = point.x;
+    CGFloat y1 = point.y;
+    //表示方形按钮的中心
+    CGFloat x2 = self.frame.size.width / 2;
+    CGFloat y2 = self.frame.size.height / 2;
+    
+    //计算当前点击的这个点距离方形中心的实际距离
+    double dis = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    // 如果这个距离小于方形按钮半径，则在圆形范围内，返回yes
+    if (dis <= self.frame.size.width / 2) {
+        return YES;
+    }
+    else{
+        return NO;
+    }
+}
+
+@end
+
+
+ViewController类
+
+ViewController.h
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
+
+
+ViewController.m
+#import "ViewController.h"
+#import "CustomButton.h"
+@interface ViewController ()
+{
+    CustomButton *cornerButton;
+}
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    cornerButton = [[CustomButton alloc] initWithFrame:CGRectMake(100, 100, 120, 120)];
+    cornerButton.backgroundColor = [UIColor blueColor];
+    [cornerButton addTarget:self action:@selector(doAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:cornerButton];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)doAction:(id)sender{
+    NSLog(@"click");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
